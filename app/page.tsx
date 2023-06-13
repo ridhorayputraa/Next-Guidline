a976c3674938e1a4f7abc5e913e42c11d11af98f
ridhorayputraa <ridhorayputra033@Gmail.com>
2023-06-13 14:03:30 +0700

    dalam 1 halaman bisa naro macam macam fetching hahaha ssr isr

--- app/page.tsx
+++ app/page.tsx
@@ -2,15 +2,16 @@ import Image from "next/image";
 import RootLayout from "./layout";
 import Link from "next/link";
 import { cache } from "react";
+import { json } from "stream/consumers";
 
 const getTimeStatic = async () => {
   const ress = await fetch(
     "https://timeapi.io/api/Time/current/zone?timeZone=Asia/Jakarta"
   );
-  return ress.json;
+  return ress.json();
 };
 
-const getTime = async () => {
+const getTimeRevalidate = async () => {
   const res = await fetch(
     // 3. -> ini adlaah bentuk nya Incrementa; data karna datanya berubah (sesuai detik)
     // ISR -> imceremental site rendering
@@ -22,15 +23,29 @@ const getTime = async () => {
       next: { revalidate: 3 },
     }
   );
-
   // console.log(res)
   return res.json();
+};
+
+const getTimeNew = async () => {
+  const ress = await fetch(
+    "https://timeapi.io/api/Time/current/zone?timeZone=Asia/Jakarta",
+    { cache: "no-cache" }
+  );
+  return ress.json();
 };
 
 // karean ingin meamnggil await maka di top function nya harus
 // di kasih async
 export default async function Home() {
-  const { dateTime } = await getTime();
+  // pakai promise alll buat nge add semua function yang ada promise nya(async)
+  // karena datanya banyak maka tampung pake array
+  // yang dibawha adalah nama variable
+  const [staticData, incrementData, noCacheData] = await Promise.all([
+    getTimeStatic(),
+    getTimeNew(),
+    getTimeRevalidate(),
+  ]);
   // console.log(time);
 
   return (
@@ -38,8 +53,25 @@ export default async function Home() {
       <main className="flex min-h-screen flex-col items-center justify-between p-24">
         <Link href="/dashboard">Ke dashboard bray</Link>
         {/* <h1>Mantapp</h1> */}
+        <p className="text-pink-800">
+          Data hasil fecth Increment Data :{incrementData.dateTime}
+        </p>
+        <p className="text-pink-800">
+          Data hasil fecth Increment Data with to iso StringSting :
+          {new Date(incrementData.dateTime).toISOString()}
+        </p>
 
-        <p>Data hasil fecth : {dateTime}</p>
+        <p className="text-blue-700">
+          Data Fetch always hot (no-cache) :{" "}
+          {new Date(noCacheData.dateTime).toISOString()}
+        </p>
+
+        <p>
+          Data hasil fecth StaticData :
+          {new Date(staticData.dateTime).toISOString()}
+        </p>
+        {/* <p>Data hasil fecth : {dateTime}</p>
+        <p>Data hasil fecth : {dateTime}</p> */}
       </main>
     </RootLayout>
   );
